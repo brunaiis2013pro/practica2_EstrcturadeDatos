9221149afd18c7c01de362d4fee463c272cb99b9
brunaiis2013pro <brunaiis2013pro@gmail.com>
2020-09-10 12:36:15 -0400

    practicaa2

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        Practica2 p2 = new Practica2();
+
+        p2.ejecutar();
+        p2.mostrar();
+    }
+}
--- src/Practica2.java
+++ src/Practica2.java
@@ -1,21 +1,17 @@
+import java.util.Scanner;
 /*
 Ejercicio 4
 Programa que declare un vector de diez elementos enteros y pida números para rellenarlo hasta que se
 llene el vector o se introduzca un número negativo. Entonces se debe imprimir el vector (sólo los
 elementos introducidos).
  */
-package practica.pkg2;
+public class Practica2 {
+    Scanner entrada = new Scanner(System.in);
 
-import java.util.Scanner;
+    int[] elementos = new int[10];
+    int cont=0;
 
-public class PRACTICA2 {
-
-    public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        
-        int[] elementos = new int[10];
-        int cont=0;
-        
+    public void ejecutar(){
         //Pedimos los 10 elementos de mi array o vector
         for(int i=0; i<10; i++){
             System.out.print((i+1)+". Digita un numero: ");
@@ -25,14 +21,14 @@ public class PRACTICA2 {
             }
             cont++;
         }
-        
+    }
+
+    public void mostrar(){
         //Imprimimos los elementos del vector
         System.out.println("\nEl arreglo es: ");
-         for(int i=0; i<cont; i++){
-          
-             System.out.print( elementos[i]+" ");
+        for(int i=0; i<cont; i++){
+
+            System.out.print( elementos[i]+" ");
         }
-        System.out.println();
     }
-    
 }
